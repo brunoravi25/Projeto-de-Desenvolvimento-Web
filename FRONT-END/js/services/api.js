5eda6026e043f443d588e982cf10b739775fa328
felipe-cardosodias <felipe.cardosodias1@gmail.com>
2021-04-16 18:35:50 -0300

    Atualizado

---
+++ FRONT-END/js/services/api.js
@@ -0,0 +1,11 @@
+const domain = 'http://localhost:3000';
+
+async function create(resource, data) {}
+
+async function read(resource) {}
+
+async function update(resource, data) {}
+
+async function destroy(resource) {}
+
+export default { create, read, update, destroy };
