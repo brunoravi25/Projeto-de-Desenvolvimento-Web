8fe9b4b35c106103096edd6eaebb7199d75dbe30
felipe-cardosodias <felipe.cardosodias1@gmail.com>
2021-04-19 18:44:12 -0300

    Modificações que fiz

--- FRONT-END/js/services/api.js
+++ FRONT-END/js/services/api.js
@@ -1,11 +1,41 @@
 const domain = 'http://localhost:3000';
 
-async function create(resource, data) {}
 
-async function read(resource) {}
+async function read(resource) {
+    const res = await fetch(`${domain}${resource}`);
+  
+    return await res.json();
+}
+  
+async function update(resource, data) {
+    const res = await fetch(`${domain}${resource}`, {
+        method: 'put',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+        },
+    });
+  
+    return await res.json();
+}
 
-async function update(resource, data) {}
+async function create(resource, data) {
+    const res = await fetch(`${domain}${resource}`, {
+      method: 'post',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    });
+  
+    return await res.json();
+  }
+  
 
-async function destroy(resource) {}
+async function destroy(resource) {
+    await fetch(`${domain}${resource}`, {
+        method: 'delete',
+    });
+}
 
 export default { create, read, update, destroy };
